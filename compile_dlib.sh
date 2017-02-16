5357c564b8d15542666d9984b049ca32481fc662
Mohammad Haghighat <haghighat@ieee.org>
2017-02-16 12:16:17 -0800

    Updated for Dlib 19.2

--- compile_dlib.sh
+++ compile_dlib.sh
@@ -1,7 +1,7 @@
 echo "compiling $1"
 if [[ $1 == *.cpp ]]
 then
-    g++ -std=c++11 -O3 -I~/Dlib/dlib/ ~/Dlib/dlib/dlib/all/source.cpp -lpthread -lX11 -o `basename $1 .cpp` $1;
+    g++ -std=c++11 -O3 -I/home/mohammad/Dlib/dlib/ /home/mohammad/Dlib/dlib/dlib/all/source.cpp -lpthread -lX11 -o `basename $1 .cpp` $1 -ldlib;
     echo "Output file => ${1%.*}"
 else
     echo "Please compile only .cpp files"
